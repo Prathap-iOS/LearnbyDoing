004fb2af3b86ad493e0c6e8ccc468f3941b6681d
NowFloats <prathap.reddy@nowfloats.com>
2023-09-26 23:13:17 +0530

    Load static data in SwiftUI

--- Learn by Doing/CardView.swift
+++ Learn by Doing/CardView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 struct CardView: View {
     // MARK: - PROPERTIES
     
-    var gradient: [Color] = [Color("Color01"), Color("Color02")]
+    var card: Card
     
     var body: some View {
         ZStack {
-            Image("developer-no1")
+            Image(card.imageName)
             
             VStack {
-                Text("SwiftUI")
+                Text(card.title)
                     .font(.largeTitle)
                     .fontWeight(.heavy)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
-                Text("Better apps. Less code.")
+                Text(card.headline)
                     .fontWeight(.light)
                     .foregroundColor(Color.white)
                     .italic()
@@ -33,7 +33,7 @@ struct CardView: View {
                 // Some action
             }, label: {
                 HStack {
-                    Text("Learn".uppercased())
+                    Text(card.callToAction.uppercased())
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
                     .accentColor(.white)
@@ -44,14 +44,14 @@ struct CardView: View {
                 } //: HSTACk
                 .padding(.vertical)
                 .padding(.horizontal, 24)
-                .background(LinearGradient(gradient: Gradient(colors: gradient), startPoint: .leading, endPoint: .trailing))
+                .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .leading, endPoint: .trailing))
                 .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             })
             .offset(y: 210)
         }
         .frame(width: 335, height: 545)
-        .background(LinearGradient(gradient: Gradient(colors: gradient), startPoint: .top, endPoint: .bottom))
+        .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(10)
         .shadow(radius: 8)
     }
@@ -61,7 +61,7 @@ struct CardView: View {
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(card: cardData[1])
             .previewLayout(.sizeThatFits)
     }
 }
