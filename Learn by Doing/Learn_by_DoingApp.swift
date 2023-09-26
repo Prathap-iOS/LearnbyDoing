65d8e0477ac6091847ed3a3ff5aa756ac7122353
NowFloats <prathap.reddy@nowfloats.com>
2023-09-26 19:00:52 +0530

    Icons, Colorsets and Launch screens added

---
+++ Learn by Doing/Learn_by_DoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_by_DoingApp.swift
+//  Learn by Doing
+//
+//  Created by Prathap Reddy on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_by_DoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
